31ba32a4d9a5f74dbbe60c5f0a67bb36170e015e
bpfefferle <brent.pfefferle@gmail.com>
2021-10-26 17:07:52 -0700

    fb messenger plugin

---
+++ assets/js/fb-messenger.js
@@ -0,0 +1,20 @@
+// FB messenger plugin.
+var chatbox = document.getElementById('fb-customer-chat');
+chatbox.setAttribute("page_id", "1596911947216618");
+chatbox.setAttribute("attribution", "biz_inbox");
+
+window.fbAsyncInit = function() {
+    FB.init({
+        xfbml            : true,
+        version          : 'v12.0'
+    });
+};
+
+(function(d, s, id)
+{
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
